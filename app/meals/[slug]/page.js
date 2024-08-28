6afd0b7e2734b6635e5bb6e3c4fd541047d46ca7
LiverPork <D00264678@student.dkit.ie>
2024-08-28 22:34:55 +0300

    small fixes

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -13,7 +13,7 @@ export default function MealsSlugPage({params}) {
         <>
             <header className={classes.header}>
                 <div className={classes.image}>
-                    <Image src={meal.image} alt={meal.title}fill />
+                    <Image src={meal.image} alt={meal.title} fill />
                 </div>
                 <div className={classes.headerText}>
                     <h1>{meal.title}</h1>
